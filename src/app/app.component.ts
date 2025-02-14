e1d7a3b6efe799203371beef73564ca8a111d331
ayaatef11 <ayabadrin667@gmail.com>
2025-02-14 18:26:20 +0200

    ss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [SidebarComponent]
 })
 export class AppComponent {
   title = 'Dashboard';
