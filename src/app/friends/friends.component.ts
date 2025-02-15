873705cc890effbd98f9abf3a31ee9769265fdba
ayaatef11 <ayabadrin667@gmail.com>
2025-02-15 18:21:20 +0200

    update profile

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { NgFor, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-friends',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [NgFor,RouterLink,NgIf],
   templateUrl: './friends.component.html',
   styleUrl: './friends.component.css'
 })
 export class FriendsComponent {
+  friends = [
+    {
+      id: 1,
+      name: 'Ahmed Nasser',
+      role: 'JavaScript Developer',
+      image: '/imgs/friend-01.jpg',
+      friendsCount: 99,
+      projects: 15,
+      articles: 25,
+      joined: '02/10/2021',
+      isVip: true
+    },
+    {
+      id: 2,
+      name: 'Omar Fathy',
+      role: 'Cloud Developer',
+      image: '/imgs/friend-02.jpg',
+      friendsCount: 30,
+      projects: 11,
+      articles: 12,
+      joined: '02/08/2020',
+      isVip: false
+    },
+    {
+      id: 3,
+      name: 'Omar Ahmed',
+      role: 'Mobile Developer',
+      image: '/imgs/friend-03.jpg',
+      friendsCount: 80,
+      projects: 20,
+      articles: 18,
+      joined: '02/06/2020',
+      isVip: false
+    }
+  ];
 
+  removeFriend(id: number) {
+    this.friends = this.friends.filter(friend => friend.id !== id);
+  }
 }
--- src/app/plans/plans.component.ts
+++ src/app/plans/plans.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { NgClass, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-plans',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [NgFor,NgClass],
   templateUrl: './plans.component.html',
   styleUrl: './plans.component.css'
 })
 export class PlansComponent {
+  features = [
+    { text: 'Access All Text Lessons', available: true },
+    { text: 'Access All Videos Lessons', available: true },
+    { text: 'Appear On Leaderboard', available: true },
+    { text: 'Browse Content Without Ads', available: false },
+    { text: 'Access All Assignments', available: false },
+    { text: 'Get Daily Prizes', available: false },
+    { text: 'Earn Certificate', available: false },
+    { text: '1 GB Space For Hosting Files', available: false },
+    { text: 'Access Badge System', available: false }
+  ];
+  plan = {
+    name: 'Premium',
+    price: 19.99,
+    features: [
+      'Access All Text Lessons',
+      'Access All Videos Lessons',
+      'Appear On Leaderboard',
+      'Browse Content Without Ads',
+      'Access All Assignments',
+      'Get Daily Prizes',
+      'Earn Certificate',
+      '1 GB Space For Hosting Files',
+      'Access Badge System'
+    ]
+  };
 
 }
