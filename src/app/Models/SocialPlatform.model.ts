e2246f94cf50db8061edfafe338cc7bd69f2254d
ayaatef11 <ayabadrin667@gmail.com>
2025-03-08 04:08:59 +0200

    create social platform model

---
+++ src/app/Models/SocialPlatform.model.ts
@@ -0,0 +1,17 @@
+export class SocialPlatform
+{
+constructor(Id:number,name:string,icon:string,followers:string,action:string,classs:string){
+this.Id=Id;
+this.name=name;
+this.icon=icon;
+this.followers=followers;
+this.action=action;
+this.classs=classs;
+}
+Id:number;
+  name:string;
+  icon:string;
+  followers:string;
+  action:string;
+  classs:string;
+}
