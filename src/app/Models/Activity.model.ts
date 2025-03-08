de0488c63f2b78533e8570549209bbd2f35addbc
ayaatef11 <ayabadrin667@gmail.com>
2025-03-08 04:04:44 +0200

    create activity model

---
+++ src/app/Models/Activity.model.ts
@@ -0,0 +1,9 @@
+export class Activity {
+  constructor(
+    public image: string,
+    public title: string,
+    public description: string,
+    public time: string,
+    public date: string
+  ) {}
+}
