71c1cd211a969b49f4bf32a407f6541a56e44d8d
ayaatef11 <ayabadrin667@gmail.com>
2025-03-08 04:07:27 +0200

    create file category

---
+++ src/app/Models/FileCategory.model.ts
@@ -0,0 +1,18 @@
+export class FileCategory{
+
+  constructor(Id:number,name:string,count:number, size:number,icon:string,color:string){
+       this.Id=Id;
+       this.name=name;
+       this.count=count;
+       this.size=size;
+       this.icon=icon;
+       this.color=color;
+  }
+  Id:number;
+  name:string;
+  count:number;
+  size:number;
+  icon:string;
+  color:string;
+
+}
