560ba8040a285dc6f8e20e51127d14f0d4047847
ayaatef11 <ayabadrin667@gmail.com>
2025-03-08 04:09:10 +0200

    create status model

---
+++ src/app/Models/Status.model.ts
@@ -0,0 +1,16 @@
+export class Status{
+
+  constructor(id:number,label:string,count:number,icon:string,color:string){
+this.id=id;
+this.label=label;
+this.count=count;
+this.icon=icon;
+this.color=color;
+  }
+  id:number;
+
+  label:string;
+  count:number;
+  icon:string;
+  color:string;
+}
