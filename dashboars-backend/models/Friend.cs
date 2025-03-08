91f7ebc7fd43e74f563cb7a57660c5c73c6f2a9f
ayaatef11 <ayabadrin667@gmail.com>
2025-03-08 04:02:47 +0200

    create friend path

---
+++ dashboars-backend/models/Friend.cs
@@ -0,0 +1,27 @@
+public class Friend
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Role { get; set; }
+    public string Image { get; set; }
+    public int FriendsCount { get; set; }
+    public int Projects { get; set; }
+    public int Articles { get; set; }
+    public DateTime Joined { get; set; }
+    public bool IsVip { get; set; }
+
+    // Constructor
+    public Friend(int id, string name, string role, string image, int friendsCount,
+                  int projects, int articles, DateTime joined, bool isVip)
+    {
+        Id = id;
+        Name = name;
+        Role = role;
+        Image = image;
+        FriendsCount = friendsCount;
+        Projects = projects;
+        Articles = articles;
+        Joined = joined;
+        IsVip = isVip;
+    }
+}
